6184de4bc3504241a7d3e3074aa9e4c28376b13d
Gabor <balintgabor04@gmail.com>
2026-09-17 16:55:40 +0200

    Kezdőlap fixálása

--- main/script.js
+++ main/script.js
@@ -2,7 +2,7 @@ const hamburgerGomb = document.querySelector('input[type="button"]');
 hamburgerGomb.addEventListener("click",showHamburgerMenu);
 
 function showHamburgerMenu() {
-    const menu = document.querySelector("#menu");
+    const menu = document.querySelector("main");
     const hamburgerMenu = document.querySelector("nav");
     if(hamburgerMenu.style.display ==="none" || hamburgerMenu.style.display ===""){
         hamburgerMenu.style.display = "block";
@@ -17,8 +17,12 @@ function updateTime(){
     const date = new Date();
     const hours = date.getHours();
     const minutes = date.getMinutes();
-    const ido = document.querySelector("#ido");
-    ido.textContent = `${hours}:${minutes}`;
+    const ido = document.querySelector("#ido p");
+    const hoursString = hours<10 ? `0${hours}` : hours;
+    const minutesString = minutes<10 ? `0${minutes}` : minutes;
+    console.log(hours);
+    
+    ido.textContent = `${hoursString}:${minutesString}`;
 }
 updateTime();
 setInterval(updateTime,300);
