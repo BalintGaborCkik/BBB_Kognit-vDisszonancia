894af53986b0695f56c686534441dd86ba793e10
Gabor <balintgabor04@gmail.com>
2026-09-19 12:57:47 +0200

    main-kriszti idofikszalas

--- hamburgerMenu.js
+++ hamburgerMenu.js
@@ -6,9 +6,7 @@ function showHamburgerMenu() {
     const hamburgerMenu = document.querySelector("nav");
     if(hamburgerMenu.style.display ==="none" || hamburgerMenu.style.display ===""){
         hamburgerMenu.style.display = "block";
-        menu.style.gridTemplateColumns = "1fr 9fr";
     }else{
         hamburgerMenu.style.display = "none";
-        menu.style.gridTemplateColumns = "1fr";
     }
 }
--- main_kriszti/script.js
+++ main_kriszti/script.js
@@ -3,7 +3,7 @@ function updateTime(){
     const date = new Date();
     const hours = date.getHours();
     const minutes = date.getMinutes();
-    const ido = document.querySelector("#ido p");
+    const ido = document.querySelector("#fehercucc");
     const hoursString = hours<10 ? `0${hours}` : hours;
     const minutesString = minutes<10 ? `0${minutes}` : minutes;
     console.log(hours);
