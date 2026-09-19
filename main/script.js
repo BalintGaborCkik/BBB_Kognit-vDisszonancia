d1d7fe08dd6e6a2f089b80dc08091fce8d53dce0
Gabor <balintgabor04@gmail.com>
2026-09-19 09:36:55 +0200

    hamburgermenu kint

--- main/script.js
+++ main/script.js
@@ -1,18 +1,3 @@
-const hamburgerGomb = document.querySelector('input[type="button"]');
-hamburgerGomb.addEventListener("click",showHamburgerMenu);
-
-function showHamburgerMenu() {
-    const menu = document.querySelector("main");
-    const hamburgerMenu = document.querySelector("nav");
-    if(hamburgerMenu.style.display ==="none" || hamburgerMenu.style.display ===""){
-        hamburgerMenu.style.display = "block";
-        menu.style.gridTemplateColumns = "1fr 9fr";
-    }else{
-        hamburgerMenu.style.display = "none";
-        menu.style.gridTemplateColumns = "1fr";
-    }
-}
-
 function updateTime(){
     const date = new Date();
     const hours = date.getHours();
