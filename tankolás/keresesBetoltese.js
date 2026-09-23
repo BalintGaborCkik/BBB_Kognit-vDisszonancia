88f07adcd182ffd7f2630f3ed09a1aaf042f0123
Gabor <balintgabor04@gmail.com>
2026-09-23 14:59:41 +0200

    suicide

--- tankolás/keresesBetoltese.js
+++ tankolás/keresesBetoltese.js
@@ -3,10 +3,17 @@ const keresesValue = JSON.parse(localStorage.getItem("kereses"));
 
 const monthTable =[31,28,31,30,31,30,31,31,30,31,30,31]
 
+function getSum(cap) {
+    let s = 0;
+    for(let i = 0;i<cap;i++){
+        s+=monthTable[i];
+    }
+    return s;
+}
 function kozteVan(akt,min,max) {
-    const aktDay = akt.year*365+monthTable[akt.month]+akt.day
-    const minDay = min.year*365+monthTable[min.month]+min.day
-    const maxDay = max.year*365+monthTable[max.month]+max.day
+    const aktDay = akt.year*365+(akt.year-(akt.year%4))/4+getSum(akt.month)+akt.day
+    const minDay = min.year*365+(min.year-(min.year%4))/4+getSum(min.month)+min.day
+    const maxDay = max.year*365+(max.year-(max.year%4))/4+getSum(max.month)+max.day
     return aktDay>=minDay && aktDay<=maxDay;
 }
 for (const tank of tankolasok) {
